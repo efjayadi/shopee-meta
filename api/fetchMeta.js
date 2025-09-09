0c766e6d7b5d29ffd2a7a65c26db125ed6e35c4a
Fajar Jayadi <89061075+efjayadi@users.noreply.github.com>
2025-09-10 01:42:05 +0700

    Update fetchMeta.js

--- api/fetchMeta.js
+++ api/fetchMeta.js
@@ -11,7 +11,9 @@ module.exports = async (req, res) => {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-        "Accept": "application/json"
+        "Accept": "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+        "X-API-SOURCE": "pc"  // 👈 this header tricks Shopee
       },
     });
 
